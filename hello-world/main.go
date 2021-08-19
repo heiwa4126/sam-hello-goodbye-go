a90b43bb72c6261b453a714822300de2e890be2c
heiwa4126 <heiwa4126@gmail.com>
2021-08-19 14:48:08 +0900

    update

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+type Message struct {
+	Message string `json:"message"`
+}
 
 func handler(res events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
@@ -27,9 +32,14 @@ func handler(res events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse,
 		return events.APIGatewayProxyResponse{}, errors.New("oops!")
 	}
 
+	s, err := json.Marshal(Message{msg})
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
 	return events.APIGatewayProxyResponse{
-		Body:       msg,
 		StatusCode: 200,
+		Body:       string(s),
 	}, nil
 }
 
